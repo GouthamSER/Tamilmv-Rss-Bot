808cafdabd96b148d87cb695a84bcec670ee8f8f
Goutham Josh <97418751+GouthamSER@users.noreply.github.com>
2026-09-22 15:35:37 +0530

    Update CHECK_INTERVAL to 480 seconds
    Increased the check interval from 300 to 480 seconds.

--- bot.py
+++ bot.py
@@ -46,7 +46,7 @@ GATEWAY_DOMAINS = [
 BASE_URL = NETWORK.BASE_URL.rstrip("/") if NETWORK.BASE_URL else "https://www.1tamilmv.rocks"
 FORUM_URL = f"{BASE_URL}/index.php?/forums/topic/"
 MAX_TOPICS = 15
-CHECK_INTERVAL = 300
+CHECK_INTERVAL = 480
 
 THUMB_URL = "https://i.ibb.co/DPrwsGsC/IMG-20260919-174828-023.jpg"
 THUMB_PATH = os.path.join(tempfile.gettempdir(), "tbl_thumb.jpg")
